5b99e1b180e1529e7fd874a2f7717dca18e69a82
moco <pps.spark.1999@gamil.com>
2021-12-23 18:13:43 +0900

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
       resources :users
     end
   end
+
+  get '/public', to: 'public#public'
+  get '/private', to: 'private#private'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
